85177e3ae5c93726235ef1884a6e7994e04c143c
nightfade <nightfade.blue@gmail.com>
2016-04-12 15:46:01 +0800

    MOD: docs & git ignore

---
+++ ios/CEE/CEE/Controllers/DrawerViewController.h
@@ -0,0 +1,13 @@
+//
+//  DrawerViewController.h
+//  CEE
+//
+//  Created by Meng on 16/4/11.
+//  Copyright © 2016年 ygeek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DrawerViewController : UIViewController
+
+@end
---
+++ ios/CEE/CEE/Controllers/WorldViewController.h
@@ -0,0 +1,13 @@
+//
+//  WorldViewController.h
+//  CEE
+//
+//  Created by Meng on 16/4/11.
+//  Copyright © 2016年 ygeek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WorldViewController : UIViewController
+
+@end
