e7b9ebccf7d9b9da4cf8884af1495995905a8768
李娴 <lixian@lixiandeMacBook-Pro-2.local>
2016-04-12 21:56:46 +0800

    by lixian change models.py

--- server/cee/api/models.py
+++ server/cee/api/models.py
@@ -3,3 +3,42 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Story(models.Model):
+    story_id = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    desc = models.TextField()
+    time = models.IntegerField()
+    good = models.IntegerField()
+    distance = models.FloatField()
+    level_ids = 
+    city = models.CharField(max_length=50)
+
+class Level(models.Model):
+    level_id = models.CharField(max_length=50)
+    level_type = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    video_url = models.URLField()
+    img_url = models.URLField()
+    test = models.TextField()
+    number_answer = models.CharField(max_length=50)
+    h5_url = models.URLField()
+
+class Item(models.Model):
+    item_id = models.CharField(max_length=50)
+    item_type = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
+    desc = models.TextField()
+    data = models.TextField()
+
+class Coupon(models.Model):
+    coupon_id = models.CharField(max_length=50)
+    gmt_start = modules.DateField()
+    gmt_end = modules.DateField()
+    name = models.CharField(max_length=50)
+    desc = models.TextField()
+    state = models.CharField(max_length=50)
+    code = models.CharField(max_length=50)
+    is_deleted = models.BooleanField()
+
+
+
--- server/cee/api/views.py
+++ server/cee/api/views.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from rest_framework import viewsets
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
