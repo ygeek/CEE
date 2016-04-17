82ab174f85a5257a05d782659442d49c55219240
nightfade <nightfade.blue@gmail.com>
2016-04-18 06:51:29 +0800

    ADD: thirdparty openid auth

--- server/cee/api/config.py
+++ server/cee/api/config.py
@@ -7,6 +7,8 @@ class LeanCloudConfig(object):
     APP_ID = 'zbamEfqUbNTXNwLKw8LiTPK0-gzGzoHsz'
     APP_KEY = 'nWuVXVcpDSr4Eu3DHJqqSDyY'
     PUSH_URL = 'https://leancloud.cn/1.1/push'
+    APP_ID_HEADER = 'X-LC-Id'
+    APP_KEY_HEADER = 'X-LC-Key'
 
 
 class QiniuConfig(object):
@@ -18,3 +20,38 @@ class QiniuConfig(object):
     BUCKET_DOMAIN_2 = '7xt08d.com2.z0.glb.clouddn.com'
     BUCKET_DOMAIN_3 = '7xt08d.com2.z0.glb.qiniucdn.com'
 
+
+class WeixinConfig(object):
+    """
+    Weixin API: https://open.weixin.qq.com/cgi-bin/showdocument
+                ?action=dir_list&t=resource/res_list&verify=1&id=open1419316518&token=&lang=zh_CN
+    """
+    AUTH_URL = 'https://api.weixin.qq.com/sns/auth'
+    ACCESS_TOKEN_KEY = 'access_token'
+    OPENID_KEY = 'openid'
+    ERRCODE_KEY = 'errcode'
+
+
+class QQConfig(object):
+    """
+    QQ API: http://wiki.open.qq.com/wiki/website/get_user_info
+
+    """
+    APPID = '12345'  # TODO (zhangmeng): 等正式APP ID
+    AUTH_URL = 'https://graph.qq.com/user/get_user_info'
+    ACCESS_TOKEN_KEY = 'access_token'
+    APPID_KEY = 'oauth_consumer_key'
+    OPENID_KEY = 'openid'
+    FORMAT_KEY = 'format'
+    JSON_FORMAT = 'json'
+    ERRCODE_KEY = 'ret'
+
+
+class WeiboConfig(object):
+    """
+    Weibo API: http://open.weibo.com/wiki/2/users/show
+    """
+    AUTH_URL = 'https://api.weibo.com/2/users/show.json'
+    ACCESS_TOKEN_KEY = 'access_token'
+    OPENID_KEY = 'uid'
+    ERRCODE_KEY = 'error_code'
--- server/cee/api/services.py
+++ server/cee/api/services.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
+from __future__ import absolute_import
 
 import json
 import requests
 
-from .config import LeanCloudConfig
+from .config import LeanCloudConfig, WeixinConfig, QQConfig, WeiboConfig
 
 
 def broadcast_apns_notification(message, is_prod=True):
@@ -37,8 +38,35 @@ def push_apns_notification(message, installation_id, is_prod=True):
 
 def _post_apns_payload(payload):
     headers = {
-        'X-LC-Id': LeanCloudConfig.APP_ID,
-        'X_LC_Key': LeanCloudConfig.APP_KEY,
+        LeanCloudConfig.APP_ID_HEADER: LeanCloudConfig.APP_ID,
+        LeanCloudConfig.APP_KEY_HEADER: LeanCloudConfig.APP_KEY,
         'Content-Type': 'application/json',
     }
     requests.post(LeanCloudConfig.PUSH_URL, data=json.dumps(payload), headers=headers)
+
+
+def verify_wexin_openid(access_token, openid):
+    response = requests.get(WeixinConfig.AUTH_URL, {
+        WeixinConfig.ACCESS_TOKEN_KEY: access_token,
+        WeixinConfig.OPENID_KEY: openid,
+    })
+    return response.json().get(WeixinConfig.ERRCODE_KEY) == 0
+
+
+def verify_weibo_openid(access_token, openid):
+    response = requests.get(WeiboConfig.AUTH_URL, {
+        WeiboConfig.ACCESS_TOKEN_KEY: access_token,
+        WeiboConfig.OPENID_KEY: openid,
+    })
+    errcode = response.json().get(WeiboConfig.ERRCODE_KEY)
+    return errcode is None or errcode == 0
+
+
+def verify_qq_openid(access_token, openid):
+    response = requests.get(QQConfig.AUTH_URL, {
+        QQConfig.ACCESS_TOKEN_KEY: access_token,
+        QQConfig.APPID_KEY: QQConfig.APPID,
+        QQConfig.OPENID_KEY: openid,
+        QQConfig.FORMAT_KEY: QQConfig.JSON_FORMAT,
+    })
+    return response.json().get(QQConfig.ERRCODE_KEY) == 0
