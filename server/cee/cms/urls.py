15d8400eb68e47008cce34842f3d388197a338b4
Yue Wang <wangyue2002@gmail.com>
2016-04-20 16:41:35 +0800

    ADD: cms user manage entry

--- server/cee/cms/urls.py
+++ server/cee/cms/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='cms-index'),
+    url(r'^users/$', views.UserList.as_view(), name='cms-users'),
+    url(r'^user/(?P<pk>[0-9]+)', views.UserDetail.as_view(), name='cms-user-detail')
 ]
--- server/cee/cms/views.py
+++ server/cee/cms/views.py
@@ -1,9 +1,28 @@
 # coding=utf-8
 
-from __future__ import unicode_literals
+from __future__ import unicode_literals, print_function
+
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.generic import ListView, DetailView
 
 
 def index(request):
     return render(request, "cms/index.html")
 
+
+@method_decorator(staff_member_required, name='dispatch')
+class UserList(ListView):
+    model = User
+    template_name = 'cms/users.html'
+    context_object_name = 'users'
+    paginate_by = 20
+
+
+@method_decorator(staff_member_required, name='dispatch')
+class UserDetail(DetailView):
+    model = User
+    template_name = 'cms/user.html'
+    context_object_name = 'user'
