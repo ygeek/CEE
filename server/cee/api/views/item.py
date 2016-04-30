28beda5424cfb6178fdf975424e61c5424650ed6
mysterylx <413448361@qq.com>
2016-04-30 19:58:09 +0800

    Update item.py
    modify UserItemList

--- server/cee/api/views/item.py
+++ server/cee/api/views/item.py
@@ -33,24 +33,11 @@ class ItemDetail(APIView):
 
 
 class UserItemList(APIView):
-    def get(self, request, user_id):
-        try:
-            user_id = int(user_id)
-            user = User.objects.get(id=user_id)
-            user_items = user.user_items.all()
-            items = [ui.item for ui in user_items]
-            serializer = ItemSerializer(items, many=True)
-            return Response({
-                'code': 0,
-                'items': serializer.data
-            })
-        except ValueError:
-            return Response({
-                'code': -1,
-                'msg': 'invalid user id: %s' % user_id
-            })
-        except User.DoesNotExist:
-            return Response({
-                'code': -2,
-                'msg': 'user not exists',
-            })
+    def get(self, request):
+        user_items = request.user.user_items.all()
+        items = [ui.item for ui in user_items]
+        serializer = ItemSerializer(items, many=True)
+        return Response({
+            'code': 0,
+            'items': serializer.data
+        })
