5373e6972a2d1b5471847f7444172b6379c7aca7
Yue Wang <wangyue2002@gmail.com>
2016-05-21 00:46:18 +0800

    FIX: anchor ref_id and type unique

--- server/cee/api/models/anchor.py
+++ server/cee/api/models/anchor.py
@@ -27,10 +27,10 @@ class Anchor(models.Model):
                                     through='UserAnchor',
                                     related_name='anchors')
 
-    # class Meta:
-    #     unique_together = (
-    #         ('type', 'ref_id'),
-    #     )
+    class Meta:
+        unique_together = (
+            ('type', 'ref_id'),
+        )
 
 
 class UserAnchor(models.Model):
